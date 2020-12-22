7565c6551e893653df37a98228bdabd087fb03e1
tomaszkolonko <tz.kolonko@gmail.com>
2020-12-22 18:49:46 +0100

    initial commit

---
+++ src/main/java/com/emotionalDictionary/EmotionalDictionary/EmotionalDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.emotionalDictionary.EmotionalDictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionalDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmotionalDictionaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emotionalDictionary/EmotionalDictionary/EmotionalDictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emotionalDictionary.EmotionalDictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionalDictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
